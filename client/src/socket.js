0df483cb93f4c0c742b424b4f37ef28084243d6c
Qarola <cyan.323@gmail.com>
2021-07-12 08:42:25 -0500

    ticket-#4

--- client/src/socket.js
+++ client/src/socket.js
@@ -6,27 +6,30 @@ import {
   addOnlineUser,
 } from "./store/conversations";
 
-const socket = io(window.location.origin);
-
-socket.on("connect", () => {
-  console.log("connected to server");
-
-  socket.on("add-online-user", (id) => {
-    store.dispatch(addOnlineUser(id));
+/**
+ * Creates a socket connection and start listening to different
+ * events.
+ * @returns Socket IO instance
+ */
+const socketConnection = () => {
+  const socket = io(window.location.origin);
+  socket.on("connect", () => {
+    console.log("connected to server");
+    
+    socket.on("add-online-user", (id) => {
+      store.dispatch(addOnlineUser(id));
+    });
+    
+    socket.on("remove-offline-user", (id) => {
+      store.dispatch(removeOfflineUser(id));
+    });
+    socket.on("new-message", (data) => {
+      store.dispatch(setNewMessage(data.message, data.sender));
+    });
   });
+  return socket;
+}
 
-  socket.on("remove-offline-user", (id) => {
-    store.dispatch(removeOfflineUser(id));
-  });
-  socket.on("new-message", (data) => {
-    store.dispatch(setNewMessage(data.message, data.sender));
-  });
-});
-
-<<<<<<< HEAD
-export default socketConnection; 
-=======
-export default socket;
->>>>>>> bb1b99b79cb1f45d9742380e0067ffd360ae013b
+export default socketConnection;
 
 
